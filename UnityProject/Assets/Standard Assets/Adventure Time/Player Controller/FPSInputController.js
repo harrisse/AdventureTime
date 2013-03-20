2d081a9f9dd6c03d58535dca8462fe1fdc8ca2aa
Sam Harris <sheeprevolution@gmail.com>
2013-03-20 17:52:54 -0400

    Character model now swaps direction along with sprite

--- UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
+++ UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
@@ -1,20 +1,23 @@
 private var motor : CharacterMotor;
 private var graphics : UnityEngine.GameObject;
+private var controller : UnityEngine.CharacterController;
 var leftTexture : UnityEngine.Texture;
 var rightTexture : UnityEngine.Texture;
 
 function Awake () {
 	motor = GetComponent(CharacterMotor);
 	graphics = GameObject.Find("Graphics");
+	controller = GetComponent(CharacterController);
 }
 
 function Update () {
 	var direction = Input.GetAxis("Horizontal");
 	if (direction < 0) {
 		graphics.renderer.material.mainTexture = leftTexture;
-		
+		controller.center.x = 0.4;
 	} else if (direction > 0) {
 		graphics.renderer.material.mainTexture = rightTexture;
+		controller.center.x = -0.4;
 	}
 	// Apply the direction to the CharacterMotor
 	motor.inputMoveDirection = new Vector3(direction, 0, 0);
