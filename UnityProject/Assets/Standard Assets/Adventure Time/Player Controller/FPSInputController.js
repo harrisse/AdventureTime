79c4314cb5332225d917b007bd354031df1ccf51
Sam Harris <sheeprevolution@gmail.com>
2013-03-20 17:26:34 -0400

    Finn can now move left and right

--- UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
+++ UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
@@ -1,12 +1,23 @@
 private var motor : CharacterMotor;
+private var graphics : UnityEngine.GameObject;
+var leftTexture : UnityEngine.Texture;
+var rightTexture : UnityEngine.Texture;
 
 function Awake () {
 	motor = GetComponent(CharacterMotor);
+	graphics = GameObject.Find("Graphics");
 }
 
 function Update () {
+	var direction = Input.GetAxis("Horizontal");
+	if (direction < 0) {
+		graphics.renderer.material.mainTexture = leftTexture;
+		
+	} else if (direction > 0) {
+		graphics.renderer.material.mainTexture = rightTexture;
+	}
 	// Apply the direction to the CharacterMotor
-	motor.inputMoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+	motor.inputMoveDirection = new Vector3(direction, 0, 0);
 	motor.inputJump = Input.GetButton("Jump");
 }
 
