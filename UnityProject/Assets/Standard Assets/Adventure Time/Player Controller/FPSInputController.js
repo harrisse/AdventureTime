1e78828ca2413ce1d0a64a3bdd1bafae622d0cdd
Sam Harris <sheeprevolution@gmail.com>
2013-03-22 10:25:46 -0400

    Left and right textures are now based on sprite sheet rather than individual images. Converted Sam Kim's sprites to a sprite map.

--- UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
+++ UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
@@ -1,7 +1,5 @@
 private var motor : CharacterMotor;
 private var graphics : UnityEngine.GameObject;
-var leftTexture : UnityEngine.Texture;
-var rightTexture : UnityEngine.Texture;
 
 function Awake () {
 	motor = GetComponent(CharacterMotor);
@@ -10,8 +8,8 @@ function Awake () {
 
 function Update () {
 	var direction = Input.GetAxis("Horizontal");
-	if (direction < 0) graphics.renderer.material.mainTexture = leftTexture;
-	else if (direction > 0) graphics.renderer.material.mainTexture = rightTexture;
+	if (direction < 0) graphics.renderer.material.mainTextureOffset.x = 0.5;
+	else if (direction > 0) graphics.renderer.material.mainTextureOffset.x = 0;
 	// Apply the direction to the CharacterMotor
 	motor.inputMoveDirection = new Vector2(direction, 0);
 	motor.inputJump = Input.GetButton("Jump");
