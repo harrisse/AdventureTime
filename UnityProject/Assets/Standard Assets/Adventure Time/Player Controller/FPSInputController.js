b133caed4ffd1c7de573d7def0769ee4a02a313e
Sam Harris <sheeprevolution@gmail.com>
2013-03-23 03:06:44 -0400

    Added basic animation functionality.
    It's currently not modular at all and may not be efficient (I am not
    entirely sure...), so don't feel like you have to use this because it's
    here, there might be much better ways to do animation out there!

--- UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
+++ UnityProject/Assets/Standard Assets/Adventure Time/Player Controller/FPSInputController.js
@@ -1,5 +1,6 @@
 private var motor : CharacterMotor;
 private var graphics : UnityEngine.GameObject;
+private var time : int = 0;
 
 function Awake () {
 	motor = GetComponent(CharacterMotor);
@@ -10,6 +11,17 @@ function Update () {
 	var direction = Input.GetAxis("Horizontal");
 	if (direction < 0) graphics.renderer.material.mainTextureOffset.x = 0.5;
 	else if (direction > 0) graphics.renderer.material.mainTextureOffset.x = 0;
+	
+	if (time >= 20) {
+		if (direction != 0) {
+			if (graphics.renderer.material.mainTextureOffset.y >= 0.75) graphics.renderer.material.mainTextureOffset.y = .0;
+			else graphics.renderer.material.mainTextureOffset.y += 0.25;
+			time = 0;
+		} else {
+		graphics.renderer.material.mainTextureOffset.y = .0;
+		}
+	} 
+	time++;
 	// Apply the direction to the CharacterMotor
 	motor.inputMoveDirection = new Vector2(direction, 0);
 	motor.inputJump = Input.GetButton("Jump");
