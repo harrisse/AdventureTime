52959da58ca90170a2000448f657f62e98eba016
Sam Harris <sheeprevolution@gmail.com>
2013-03-27 16:53:02 -0400

    Updated Worm AI

--- UnityProjectSpriteManager/Assets/Scripts/WormScript.js
+++ UnityProjectSpriteManager/Assets/Scripts/WormScript.js
@@ -1,17 +1,17 @@
 #pragma strict
 private var graphics : UnityEngine.GameObject;
 private var play : UnityEngine.GameObject;
-public var moveSpeed : float = 10f;
+private var motor : CharacterMotor;
 
-function Start () {
+function Awake () {
+	motor = GetComponent(CharacterMotor);
+	graphics = GameObject.Find("PlayerGraphics");
 	play = GameObject.FindGameObjectWithTag("Player");
 }
 
-function Update () { 
-    if(play.transform.position.x > transform.position.x)
-        transform.Translate(Time.deltaTime*3, 0, 0, null);
-    
-    else//(play.transform.position.x < transform.position.x)
-        transform.Translate(-Time.deltaTime*3,0,0,null);
- 
+function Update () {
+	if (Mathf.Abs(play.transform.position.x - transform.position.x) > 0.1) motor.inputMoveDirection = (new Vector2(play.transform.position.x - transform.position.x, 0)).normalized;
+	else motor.inputMoveDirection = Vector2.zero;
 }
+
+@script RequireComponent (CharacterMotor)
