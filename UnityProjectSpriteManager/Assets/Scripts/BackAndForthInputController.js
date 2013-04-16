8a51017f98c7e7932cbab24908ea53be92c34fa8
Sam Harris <sheeprevolution@gmail.com>
2013-04-16 18:35:19 -0400

    Made some updates to BackAndForthController
    Time is now in seconds, and physics and collisions will be obeyed. The
    controller will travel in each direction by an amount equal to
    CharacterMotor.MaxSpeed * BackAndForthController.Time in game distance
    units.

--- UnityProjectSpriteManager/Assets/Scripts/BackAndForthInputController.js
+++ UnityProjectSpriteManager/Assets/Scripts/BackAndForthInputController.js
@@ -2,53 +2,26 @@
 private var graphics : UnityEngine.GameObject;
 private var play : UnityEngine.GameObject;
 private var motor : CharacterMotor;
-private var following : int;
-private var invincible : int;
-var xSpeed : float;
-var ySpeed : float;
-var time : int;
-
-private var temp : int;
-
+private var following : int = 0;
+private var temp : int = 0;
+// Whether we should go left to start or right to start.
+var goLeft : boolean = false;
+// Time is in seconds to travel each direction.
+var time : float = 3f;
+// Initialize variables
 function Awake () {
-	invincible = 0;
-	following = 0;
 	motor = GetComponent(CharacterMotor);
 	graphics = GameObject.Find("PlayerGraphics");
 	play = GameObject.FindGameObjectWithTag("Player");
+	if (goLeft) temp = time / Time.fixedDeltaTime;
 }
-
-function Update () {
-	if(invincible != 0){
-		invincible--;
-	}
-	if (temp >= time) temp = -time;
+// Use FixedUpdate here or else our worms will travel further as FPS decreases
+function FixedUpdate () {
+	if (temp >= 2 * time / Time.fixedDeltaTime) temp = 0;
 	temp++;
-	if(Mathf.Abs(play.transform.position.x - transform.position.x) < 0.6 && Mathf.Abs(play.transform.position.y - transform.position.y) < 2.5 && invincible == 0){
-		play.gameObject.GetComponent(FPSInputController).takeDamage();
-		invincible = 10;
-	}
-	if (temp > 0) {
-		transform.position.x += xSpeed;
-		transform.position.y += ySpeed;
-	} else{
-		transform.position.x -= xSpeed;
-		transform.position.y -= ySpeed;
-	}
-}
-	/*if (Mathf.Abs(play.transform.position.x - transform.position.x) < 10){
-		following = 1;
-	} else if (Mathf.Abs(play.transform.position.x - transform.position.x) > 30) {
-		following = 0;
-	}
-
-	if (Mathf.Abs(play.transform.position.x - transform.position.x) > 0.1 && following == 1) {
-		motor.inputMoveDirection = (new Vector2(play.transform.position.x - transform.position.x, 0)).normalized;
-	}
-	else motor.inputMoveDirection = Vector2.zero;
 	
-	if (!motor.inputJump && Mathf.Abs(play.transform.position.x - transform.position.x) < 5) motor.inputJump = play.transform.position.y - transform.position.y > 1;
-	else motor.inputJump = false;
-}*/
+	if (temp > time / Time.fixedDeltaTime) motor.inputMoveDirection = -Vector2.right;
+	else motor.inputMoveDirection = Vector2.right;
+}
 
 @script RequireComponent (CharacterMotor)
